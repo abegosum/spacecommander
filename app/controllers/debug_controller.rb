20f9abc4b272845fcb74b90e423cdab9f15cb7ba
Aaron Bond <abond@mpr.org>
2017-09-05 17:08:58 -0500

    Rendered chart in tb

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -4,13 +4,13 @@ class DebugController < ApplicationController
     NetappEnvironment.clusters.each do |host, vif|
       @physical_totals_graph[host] = 0
       vif.aggregates.each do |aggregate|
-        @physical_totals_graph[host] += aggregate.size.to_gb
+        @physical_totals_graph[host] += aggregate.size.to_tb
       end
     end
     NetappEnvironment.sevenmode_nodes.each do |host, node|
       @physical_totals_graph[host] = 0
       node.aggregates.each do |aggregate|
-        @physical_totals_graph[host] += aggregate.size.to_gb
+        @physical_totals_graph[host] += aggregate.size.to_tb
       end
     end
   end
