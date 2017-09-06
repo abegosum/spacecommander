b3e5f1f347d9bffa5f36c60f98755ce77a15486c
Aaron Bond <abond@mpr.org>
2017-09-05 21:34:36 -0500

    Added by location totals to debug controller

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -14,5 +14,14 @@ class DebugController < ApplicationController
         @physical_totals_graph[host] += aggregate.size.to_tb
       end
     end
+    @physical_totals_graph_by_location = {}
+    @netapp_environment.locations.each do |location, api_servers|
+      @physical_totals_graph_by_location[location] = 0
+      api_servers.each do |host, node|
+        node.aggregates.each do |aggregate|
+          @physical_totals_graph_by_location[location] += aggregate.size.to_tb
+        end
+      end
+    end
   end
 end
